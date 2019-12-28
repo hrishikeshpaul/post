60816433fc8fc117b5fb09db8f8c9c642ced3604
Hrishikesh Paul <hrishikeshpaul12@gmail.com>
2019-12-29 02:52:57 +0530

    removed redundant  comments

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
-// var express = require('express');
-// var path = require('path');
-// var serveStatic = require('serve-static');
-// app = express();
-
-// app.use(serveStatic(__dirname + "/dist"));
-
-
-// var port = process.env.PORT || 5000;
-// app.listen(port);
-// console.log('server started '+ port);
-
 const express = require('express');
 const path = require('path');
 const history = require('connect-history-api-fallback');
